195f8783c236b9dcb40ea78f6b13a3ac48e44407
Gil Pedersen <git@gpost.dk>
2022-08-08 21:54:58 +0200

    Refactor ext parsing

--- src/parser.ts
+++ src/parser.ts
@@ -14,18 +14,105 @@ export interface ParserOptions {
 }
 
 
+interface ParserState {
+    readonly m3u8: Partial<Omit<MediaPlaylist, 'master'> & Omit<MainPlaylist, 'master'> & { master: boolean }>;
+    meta: MediaSegment & { info?: AttrList };
+}
+
+
+const extParser = new Map<string,(state: ParserState, arg?: string) => void>([
+    /* eslint-disable no-return-assign */
+    ['VERSION', (_, arg) => _.m3u8.version = parseInt(arg!, 10)],
+    ['TARGETDURATION', (_, arg) => _.m3u8.target_duration = parseInt(arg!, 10)],
+    ['MEDIA-SEQUENCE', (_, arg) => _.m3u8.media_sequence = parseInt(arg!, 10)],
+    ['DISCONTINUITY-SEQUENCE', (_, arg) => _.m3u8.discontinuity_sequence = parseInt(arg!, 10)],
+    ['PLAYLIST-TYPE', (_, arg) => _.m3u8.type = arg!],
+    ['START', (_, arg) => _.m3u8.start = new AttrList(arg!)],
+    ['INDEPENDENT-SEGMENTS', (_) => _.m3u8.independent_segments = true],
+    ['ENDLIST', (_) => _.m3u8.ended = true],
+    ['KEY', (_, arg) => (_.meta.keys ??= []).push(new AttrList(arg))],
+    ['PROGRAM-DATE-TIME', (_, arg) => _.meta.program_time = new Date(arg!)],
+    ['DISCONTINUITY', (_) => _.meta.discontinuity = true],
+
+    // main
+    ['STREAM-INF', (_, arg) => {
+
+        _.m3u8.master = true;
+        _.meta.info = new AttrList(arg);
+    }],
+
+    // main v4 since main streams are not required to specify version
+    ['MEDIA', (_, arg) => {
+
+        const attrs = new AttrList(arg);
+        const id = attrs.get('group-id', AttrList.Types.String) ?? '#';
+
+        let list: AttrList[] & { type?: string } | undefined = (_.m3u8.groups ??= new Map()).get(id);
+        if (!list) {
+            list = [];
+            _.m3u8.groups.set(id, list);
+            if (id !== '#') {
+                list.type = attrs.get('type');
+            }
+        }
+
+        list.push(attrs);
+    }],
+    ['I-FRAME-STREAM-INF', (_, arg) => (_.m3u8.iframes ??= []).push(new AttrList(arg))],
+    ['SESSION-DATA', (_, arg) => {
+
+        const attrs = new AttrList(arg);
+        const id = attrs.quotedString('data-id');
+
+        if (id) {
+            let list = (_.m3u8.data ??= new Map()).get(id);
+            if (!list) {
+                list = [];
+                _.m3u8.data.set(id, list);
+            }
+
+            list.push(attrs);
+        }
+    }],
+    ['SESSION-KEY', (_, arg) => (_.m3u8.session_keys ??= []).push(new AttrList(arg))],
+    ['GAP', (_) => _.meta.gap = true],
+    ['DEFINE', (_, arg) => (_.m3u8.defines ??= []).push(new AttrList(arg))],
+    ['PART-INF', (_, arg) => _.m3u8.part_info = new AttrList(arg)],
+    ['PART', (_, arg) => (_.meta.parts ??= []).push(new AttrList(arg))],
+    ['SERVER-CONTROL', (_, arg) => _.m3u8.server_control = new AttrList(arg)],
+    ['I-FRAMES-ONLY', (_) => _.m3u8.i_frames_only = true],
+    ['BYTERANGE', (_, arg) => {
+
+        const n = arg!.split('@');
+        _.meta.byterange = { length: parseInt(n[0], 10) };
+        if (n.length > 1) {
+            _.meta.byterange.offset = parseInt(n[1], 10);
+        }
+    }],
+    ['MAP', (_, arg) => _.meta.map = new AttrList(arg)],
+    ['SKIP', (_, arg) => (_.m3u8.meta ??= Object.create(null)).skip = new AttrList(arg)]
+    /* eslint-enable no-return-assign */
+]);
+
+for (const [ext, entry] of MediaPlaylist._metas.entries()) {
+    extParser.set(ext, (_, arg) => {
+
+        const m3u8meta = _.m3u8.meta ??= Object.create(null);
+        (m3u8meta[entry] ??= []).push(new AttrList(arg));
+    });
+}
+
+
 export class M3U8Parser {
 
     // eslint-disable-next-line @typescript-eslint/no-empty-function
     static debug(line: string, ...args: unknown[]): void {}
 
-    readonly m3u8: Partial<Omit<MediaPlaylist, 'master'> & Omit<MainPlaylist, 'master'> & { master: boolean }> = {};
     readonly extensions: NonNullable<ParserOptions['extensions']>;
-    extParser: { [ext: string]: (arg: string) => void } = {};
 
     // Parser state
 
-    meta = {} as MediaSegment & { info?: AttrList };
+    state: ParserState = { m3u8: {}, meta: {} as any };
     lineNo = 0;
 
     constructor(options: ParserOptions = {}) {
@@ -33,7 +120,6 @@ export class M3U8Parser {
         this.debug = options.debug ?? M3U8Parser.debug;
 
         this.extensions = Object.assign({}, options.extensions);
-        this._prepareExtParser();
     }
 
     feed(line?: string): void {
@@ -50,15 +136,15 @@ export class M3U8Parser {
     finalize(type?: PlaylistType | `${PlaylistType}`): M3U8Playlist;
     finalize(type?: PlaylistType | `${PlaylistType}`): M3U8Playlist {
 
-        const { m3u8 } = this;
+        const { state } = this;
 
         if (this.lineNo === 0) {
             throw new ParserError('No line data');
         }
 
-        if (Object.keys(this.meta).length) {
-            (m3u8.segments ??= []).push(new MediaSegment(undefined, this.meta, m3u8.version));    // Append a partial segment
-            this.meta = {} as any;
+        if (Object.keys(state.meta).length) {
+            (state.m3u8.segments ??= []).push(new MediaSegment(undefined, state.meta, state.m3u8.version));    // Append a partial segment
+            state.meta = {} as any;
         }
 
         if (type) {
@@ -66,181 +152,20 @@ export class M3U8Parser {
                 throw new TypeError(`Passed type must be "${PlaylistType.Main}" or "${PlaylistType.Media}"`);
             }
 
-            if (!!m3u8.master !== (type === PlaylistType.Main)) {
+            if (!!state.m3u8.master !== (type === PlaylistType.Main)) {
                 throw new ParserError('Incorrect playlist type');
             }
         }
 
-        return m3u8.master ? new MainPlaylist(m3u8 as any) : new MediaPlaylist(m3u8 as any);
+        return state.m3u8.master ? new MainPlaylist(state.m3u8 as any) : new MediaPlaylist(state.m3u8 as any);
     }
 
     // eslint-disable-next-line @typescript-eslint/no-empty-function
     debug(line: string, ...args: unknown[]) {}
 
-    _prepareExtParser() {
-
-        const { m3u8 } = this;
-
-        // TODO: add more validation logic
-        this.extParser = {
-            '#EXT-X-VERSION': (arg) => {
-
-                m3u8.version = parseInt(arg, 10);
-            },
-            '#EXT-X-TARGETDURATION': (arg) => {
-
-                m3u8.target_duration = parseInt(arg, 10);
-            },
-            '#EXT-X-MEDIA-SEQUENCE': (arg) => {
-
-                m3u8.media_sequence = parseInt(arg, 10);
-            },
-            '#EXT-X-DISCONTINUITY-SEQUENCE': (arg) => {
-
-                m3u8.discontinuity_sequence = parseInt(arg, 10);
-            },
-            '#EXT-X-PLAYLIST-TYPE': (arg) => {
-
-                m3u8.type = arg;
-            },
-            '#EXT-X-START': (arg) => {
-
-                m3u8.start = new AttrList(arg);
-            },
-            '#EXT-X-INDEPENDENT-SEGMENTS': () => {
-
-                m3u8.independent_segments = true;
-            },
-            '#EXT-X-ENDLIST': () => {
-
-                m3u8.ended = true;
-            },
-
-            '#EXTINF': (arg) => {
-
-                const [duration, ...title] = arg.split(',');
-                this.meta.duration = parseFloat(duration);
-                this.meta.title = title.join(',');
-
-                if (this.meta.duration <= 0) {
-                    throw new ParserError('Invalid duration', '#EXTINF:' + arg, this.lineNo);
-                }
-            },
-            '#EXT-X-KEY': (arg) => {
-
-                if (!this.meta.keys) {
-                    this.meta.keys = [];
-                }
-
-                this.meta.keys.push(new AttrList(arg));
-            },
-            '#EXT-X-PROGRAM-DATE-TIME': (arg) => {
-
-                this.meta.program_time = new Date(arg);
-            },
-            '#EXT-X-DISCONTINUITY': () => {
-
-                this.meta.discontinuity = true;
-            },
-
-            // master
-            '#EXT-X-STREAM-INF': (arg) => {
-
-                m3u8.master = true;
-                this.meta.info = new AttrList(arg);
-            },
-            // master v4 since master streams are not required to specify version
-            '#EXT-X-MEDIA': (arg) => {
-
-                const attrs: AttrList = new AttrList(arg);
-                const id = attrs.get('group-id', AttrList.Types.String) ?? '#';
-
-                let list: AttrList[] & { type?: string } | undefined = (m3u8.groups ??= new Map()).get(id);
-                if (!list) {
-                    list = [];
-                    m3u8.groups.set(id, list);
-                    if (id !== '#') {
-                        list.type = attrs.get('type');
-                    }
-                }
-
-                list.push(attrs);
-            },
-            '#EXT-X-I-FRAME-STREAM-INF': (arg) => {
-
-                (m3u8.iframes ??= []).push(new AttrList(arg));
-            },
-            '#EXT-X-SESSION-DATA': (arg) => {
-
-                const attrs = new AttrList(arg);
-                const id = attrs.quotedString('data-id');
-
-                if (id) {
-                    let list = (m3u8.data ??= new Map()).get(id);
-                    if (!list) {
-                        list = [];
-                        m3u8.data.set(id, list);
-                    }
-
-                    list.push(attrs);
-                }
-            },
-            '#EXT-X-SESSION-KEY': (arg) => {
-
-                (m3u8.session_keys ??= []).push(new AttrList(arg));
-            },
-            '#EXT-X-GAP': () => {
-
-                this.meta.gap = true;
-            },
-            '#EXT-X-DEFINE': (arg) => {
-
-                (m3u8.defines ??= []).push(new AttrList(arg));
-            },
-            '#EXT-X-PART-INF': (arg) => {
-
-                m3u8.part_info = new AttrList(arg);
-            },
-            '#EXT-X-PART': (arg) => {
-
-                (this.meta.parts = this.meta.parts || []).push(new AttrList(arg));
-            },
-            '#EXT-X-SERVER-CONTROL': (arg) => {
-
-                m3u8.server_control = new AttrList(arg);
-            },
-            '#EXT-X-I-FRAMES-ONLY': () => {
-
-                m3u8.i_frames_only = true;
-            },
-            '#EXT-X-BYTERANGE': (arg) => {
-
-                const n = arg.split('@');
-                this.meta.byterange = { length: parseInt(n[0], 10) };
-                if (n.length > 1) {
-                    this.meta.byterange.offset = parseInt(n[1], 10);
-                }
-            },
-            '#EXT-X-MAP': (arg) => {
-
-                this.meta.map = new AttrList(arg);
-            },
-            '#EXT-X-SKIP': (arg) => {
-
-                (m3u8.meta ??= Object.create(null)).skip = new AttrList(arg);
-            }
-        };
-
-        for (const [ext, entry] of MediaPlaylist._metas.entries()) {
-            this.extParser['#EXT-X-' + ext] = (arg) => {
-
-                const m3u8meta = m3u8.meta ??= Object.create(null);
-                (m3u8meta[entry] ??= []).push(new AttrList(arg));
-            };
-        }
-    }
-
     _parseLine(line: string): void {
+
+        const { state } = this;
 
         this.lineNo += 1;
 
@@ -269,28 +194,30 @@ export class M3U8Parser {
                 return this.debug('ignoring unknown #EXT:' + cmd, this.lineNo);
             }
         }
-        else if (this.m3u8.master) {
-            this.meta.uri = line;
-            (this.m3u8.variants ??= []).push({ uri: this.meta.uri, info: this.meta.info }); // FIXME: ??
-            this.meta = {} as any;
+        else if (state.m3u8.master) {
+            state.meta.uri = line;
+            (state.m3u8.variants ??= []).push({ uri: state.meta.uri, info: state.meta.info }); // FIXME: ??
+            state.meta = {} as any;
         }
         else {
-            if (!('duration' in this.meta)) {
+            if (!('duration' in state.meta)) {
                 throw new ParserError('Missing #EXTINF before media file URI', line, this.lineNo);
             }
 
-            (this.m3u8.segments ??= []).push(new MediaSegment(line, this.meta, this.m3u8.version));
-            this.meta = {} as any;
+            (state.m3u8.segments ??= []).push(new MediaSegment(line, state.meta, state.m3u8.version));
+            state.meta = {} as any;
         }
 
     }
 
     _parseExt(cmd: string, arg: string | null = null) {
 
+        const { state } = this;
+
         // Parse vendor extensions
 
         if (cmd in this.extensions) {
-            const extObj = this.extensions[cmd] ? this.meta : this.m3u8;
+            const extObj = this.extensions[cmd] ? state.meta : state.m3u8;
             if (!extObj.vendor) {
                 extObj.vendor = [];
             }
@@ -299,12 +226,31 @@ export class M3U8Parser {
             return true;
         }
 
-        if (!(cmd in this.extParser)) {
+        if (!cmd.startsWith('#EXT-X-')) {
+            if (arg && cmd === '#EXTINF') {
+                const [duration, ...title] = arg.split(',');
+                state.meta.duration = parseFloat(duration);
+                state.meta.title = title.join(',');
+
+                if (state.meta.duration <= 0) {
+                    throw new ParserError('Invalid duration', '#EXTINF:' + arg, this.lineNo);
+                }
+
+                return true;
+            }
+
+            return false;
+        }
+
+        const name = cmd.slice(7);
+
+        const handler = extParser.get(name);
+        if (!handler) {
             return false;
         }
 
         this.debug('parsing ext', cmd, arg);
-        this.extParser[cmd](arg!);
+        handler(this.state, arg!);
 
         return true;
     }
